da202adb39730866ef1cc72cdc7983ffa033e441
Tamo <irevoire@protonmail.ch>
2026-04-02 22:54:36 +0200

    add a facet json codec

---
+++ src/codec/facet_json.rs
@@ -0,0 +1,29 @@
+use std::marker::PhantomData;
+
+use facet::Facet;
+use facet_format::SerializeError;
+use facet_json::{DeserializeError, JsonSerializeError};
+use fjall::Slice;
+
+use crate::{Decode, Encode};
+
+pub struct FacetJson<T>(PhantomData<T>);
+
+impl<T: Facet<'static>> Encode for FacetJson<T> {
+    type Item = T;
+    type Error = SerializeError<JsonSerializeError>;
+
+    fn encode(item: &Self::Item) -> Result<Slice, Self::Error> {
+        let buf = facet_json::to_vec(item)?;
+        Ok(buf.into())
+    }
+}
+
+impl<T: Facet<'static>> Decode for FacetJson<T> {
+    type Item = T;
+    type Error = DeserializeError;
+
+    fn decode(bytes: Slice) -> Result<Self::Item, Self::Error> {
+        facet_json::from_slice(&bytes)
+    }
+}
--- src/codec/mod.rs
+++ src/codec/mod.rs
@@ -6,3 +6,10 @@ mod str;
 pub use str::*;
 mod unit;
 pub use unit::*;
+#[cfg(feature = "facet_json")]
+mod facet_json;
+#[cfg(feature = "facet_json")]
+pub use facet_json::*;
+
+/// Dummy codec if you don't know yet which codec will be used
+pub enum Unspecified {}
