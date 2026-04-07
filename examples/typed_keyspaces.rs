5d4d5bd156776203cb1e47102153d9af5a10e399
Tamo <irevoire@protonmail.ch>
2026-04-07 19:20:48 +0200

    Fix the example

--- examples/typed_keyspaces.rs
+++ examples/typed_keyspaces.rs
@@ -11,11 +11,18 @@ fn main() {
     let ks = db
         .keyspace("my_items", KeyspaceCreateOptions::default)
         .unwrap();
+    // Here we wrap the original keyspace into a fjall_typed keyspace.
+    // This one indicates that it maps `u8` with strings.
     let ks = Keyspace::<U8, Str>::new(ks);
 
     ks.insert(&33, "hello").unwrap();
+
+    // The whole keyspace doesn't have to follow these types for all its keys and values.
+    // You can also remap the value or key.
     ks.remap_value::<FacetJson<String>>()
         .insert(&45, &String::from("hello"))
         .unwrap();
+
+    // This wouldn't work as "a" is not a `u8`.
     // ks.insert("a", "hello").unwrap();
 }
