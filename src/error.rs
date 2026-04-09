0d65c1d8bfa0e987a77232a7c496e1c393c6f66f
Tamo <irevoire@protonmail.ch>
2026-04-09 18:32:05 +0200

    Document the error

--- src/error.rs
+++ src/error.rs
@@ -1,13 +1,19 @@
 use std::{error::Error as StdError, fmt};
 
+/// Main error type of this crate.
+/// When possible we return a `fjall::Error` directly, but if we have to encode or decode a value we have to return the associated error.
 #[derive(Debug)]
 pub enum Error<KeyError, ValueError> {
+    /// An original fjall error. See [`fjall::Error`] for more info.
     Fjall(fjall::Error),
+    /// An encoding or decoding error happened while working with the key codec.
     Key(KeyError),
+    /// An encoding or decoding error happened while working with the value codec.
     Value(ValueError),
 }
 
-impl<KeyError: StdError, ValueError: StdError> Error<KeyError, ValueError> {
+impl<KeyError, ValueError> Error<KeyError, ValueError> {
+    /// Unwrap the [`fjall::Error`] and panic if it was another variant.
     pub fn unwrap_fjall(self) -> fjall::Error {
         match self {
             Error::Fjall(error) => error,
