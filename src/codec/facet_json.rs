e6c34be8fa8eea089071c52bfa4a36bf4ffbed63
Tamo <irevoire@protonmail.ch>
2026-04-10 13:37:33 +0200

    add a test for facet_json

--- src/codec/facet_json.rs
+++ src/codec/facet_json.rs
@@ -28,3 +28,35 @@ impl<T: Facet<'static>> Decode for FacetJson<T> {
         facet_json::from_slice(&bytes)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use facet::Facet;
+
+    use crate::codec::{Decode, Encode, FacetJson};
+
+    #[test]
+    fn encode_and_decode() {
+        #[derive(Facet, Debug, PartialEq)]
+        struct Example {
+            name: String,
+            value: i32,
+        }
+
+        let value = Example {
+            name: "pi".to_string(),
+            value: 31415926,
+        };
+
+        let facet_bytes = facet_json::to_vec(&value).unwrap();
+        let facet_deserialized = facet_json::from_slice(&facet_bytes).unwrap();
+
+        let codec_bytes = FacetJson::<Example>::encode(&value).unwrap();
+        assert_eq!(codec_bytes, facet_bytes);
+
+        let codec_deserialized = FacetJson::<Example>::decode(codec_bytes).unwrap();
+
+        assert_eq!(codec_deserialized, facet_deserialized);
+        assert_eq!(codec_deserialized, value);
+    }
+}
